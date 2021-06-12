94a88777880f8db7fb1050560e22ddc24a7faba4
Ninel <inel2008@bk.ru>
2021-06-12 20:57:59 +0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? 'https://ninelka.github.io/random-quote-machine/' : './',
+  build: {
+    outDir: 'docs'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src')
+    }
+  },
   plugins: [reactRefresh()]
 })
